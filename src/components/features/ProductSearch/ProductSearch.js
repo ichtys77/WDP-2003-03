cf8530c4534f51915bc084c895ae307cb02bbd8b
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-03-11 11:41:02 +0100

    Add ul>li to ProductSearch compoonent, problem with style css

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-icons';
-
 import styles from './ProductSearch.module.scss';
+import initialState from './../../../redux/initialState';
 
 const ProductSearch = () => (
   <form action='' className={styles.root}>
     <div className={styles.category}>
-      <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <select name='' id=''>
-        <option value=''>Select a category</option>
-      </select>
+      <FontAwesomeIcon icon={faListUl} className={styles.icon} />
+      <ul>
+        {' '}
+        Select a category
+        {initialState.categories.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
       <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
     </div>
     <div className={styles.searchField}>
