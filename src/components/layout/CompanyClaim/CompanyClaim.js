d8929d9570b8d7965cbc7e98b0caa4ad4e32a6e8
Morormo <morormo@gmail.com>
2020-03-12 00:31:59 +0100

    ADD RWD topbar and banner

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -10,18 +10,20 @@ const CompanyClaim = () => (
   <div className={styles.root}>
     <div className='container'>
       <div className='row align-items-center'>
-        <div className={`col text-left ${styles.phoneNumber}`}>
+        <div
+          className={`col-12 order-3 text-right col-md-4 text-md-left order-md-1 ${styles.phoneNumber}`}
+        >
           <p>
             <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
             222
           </p>
         </div>
-        <div className='col text-center'>
+        <div className='col-6 col-md-3 text-md-center order-md-2'>
           <a href='#'>
             <img src='/images/logo.png' alt='Bazar' />
           </a>
         </div>
-        <div className={`col text-right ${styles.cart}`}>
+        <div className={`col text-right order-md-3 ${styles.cart}`}>
           <a href='#' className={styles.cartBox}>
             <div className={styles.cartIcon}>
               <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
