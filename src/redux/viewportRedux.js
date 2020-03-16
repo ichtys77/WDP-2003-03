47877e7dffadfebb7429eb3fb76f2427465f1d1a
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-03-16 15:16:51 +0100

    ADD MainLayoutContainer && viewportRedux

---
+++ src/redux/viewportRedux.js
@@ -0,0 +1,20 @@
+/* selectors */
+export const getView = ({ viewport }) => viewport;
+
+/* action name creator */
+const reducerName = 'viewport';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+/* action types */
+const ADD_VIEWPORT = createActionName('ADD_VIEWPORT');
+
+/* action creators */
+export const addViewport = payload => ({ payload, type: ADD_VIEWPORT });
+
+/* reducer */
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+}
