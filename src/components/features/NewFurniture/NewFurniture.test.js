11428ed82e0a5079cefc5df3535352fa9fbf6889
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-03-17 18:15:13 +0100

    ADD NewFurniture unit tests

---
+++ src/components/features/NewFurniture/NewFurniture.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewFurniture from './NewFurniture';
+
+const mockProps = {
+  viewport: [{ mode: 'desktop' }, { mode: 'tablet' }, { mode: 'mobile' }],
+};
+
+describe('Component NewFurniture with mocked viewport', () => {
+  it('should render for desktop width', () => {
+    const component = shallow(<NewFurniture viewport={mockProps.viewport[0]} />);
+    expect(component).toBeTruthy();
+  });
+
+  it('should render tablet width', () => {
+    const component = shallow(<NewFurniture viewport={mockProps.viewport[1]} />);
+    expect(component).toBeTruthy();
+  });
+
+  it('should render for mobile width', () => {
+    const component = shallow(<NewFurniture viewport={mockProps.viewport[2]} />);
+    expect(component).toBeTruthy();
+  });
+});
