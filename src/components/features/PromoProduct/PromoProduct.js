6509fd685588bd34768b9b9cd6ba21b6b9479817
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-03-19 21:37:13 +0100

    SEPARATION others components from FurnitureGallery

--- src/components/features/PromoProduct/PromoProduct.js
+++ src/components/features/PromoProduct/PromoProduct.js
@@ -7,19 +7,21 @@ import Button from '../../common/Button/Button';
 class PromoProduct extends React.Component {
   render() {
     return (
-      <div className={styles.root}>
-        <div className={styles.promo}>
-          <img
-            src='https://images.pexels.com/photos/963486/pexels-photo-963486.jpeg?cs=srgb&dl=green-wooden-chair-on-white-surface-963486.jpg&fm=jpg'
-            alt='chair'
-          ></img>
-          <div className={styles.info}>
-            <p>
-              From
-              <span>$50.80</span>
-            </p>
-            <h2>Modern Chair</h2>
-            <Button variant='cta'>Shop now</Button>
+      <div className={'col-12 col-md-6 ' + styles.promotion}>
+        <div className={styles.root}>
+          <div className={styles.promo}>
+            <img
+              src='https://images.pexels.com/photos/709767/pexels-photo-709767.png?cs=srgb&dl=person-taking-photo-of-gray-pendant-lamp-over-brown-wooden-709767.jpg&fm=jpg'
+              alt='chair'
+            ></img>
+            <div className={styles.info}>
+              <p>
+                From
+                <span>$50.80</span>
+              </p>
+              <h2>Modern Chair</h2>
+              <Button variant='cta'>Shop now</Button>
+            </div>
           </div>
         </div>
       </div>
