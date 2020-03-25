99fd7e591a038b3ad3f999528dd632fd00fc2354
Anna Kuszal <anna.kuszal@o2.pl>
2020-03-25 15:26:13 +0100

    Add code for creating slides

--- src/components/features/FeaturedProducts/FeaturedProducts.js
+++ src/components/features/FeaturedProducts/FeaturedProducts.js
@@ -5,7 +5,9 @@ import styles from './FeaturedProducts.module.scss';
 import Button from '../../common/Button/Button';
 import FeaturedProductsBox from '../../common/FeaturedProductsBox/FeaturedProductsBox';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class FeaturedProducts extends React.Component {
   static propTypes = {
@@ -23,33 +25,68 @@ class FeaturedProducts extends React.Component {
       promotion: PropTypes.bool,
       image: PropTypes.string,
     })
-    )
-  }
-
-  static defaultProps = {
-    title: 'INDOOR',
-    titleBold: 'FURNITURE',
-    subtitle: 'SAVE UP TO 50% OF ALL FURNITURE',
-    btnName: 'SHOP NOW',
-    imgAlt: 'discount',
-    imgBg: 'https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    ),
+    slides: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        title: PropTypes.string,
+        titleBold: PropTypes.string,
+        subtitle: PropTypes.string,
+        btnName: PropTypes.string,
+        imgAlt: PropTypes.string,
+        image: PropTypes.string,
+      })
+      )
   }
   
-  /*
-  state = { };
-  */
+  state = { 
+    activeSlideSmall: 0,
+    activeSlidePage: 0,
+
+    fade: 0,
+  };
+
+  handleSlideSmChange(newSlide) {
+    this.setState({ activeSlideSmall: newSlide });
+  }
+
+  handleSlidePageChange(e, newSlidePage) {
+    e.preventDefault();
+
+    if (newSlidePage < 0) {
+      this.setState({ activeSlidePage: 3 });
+    } else if (newSlidePage > 3) {
+      this.setState({ activeSlidePage: 0 });
+    } else {
+      this.setState({ 
+        activeSlidePage: newSlidePage,
+        fade: this.state.fade + 1,
+       });
+    } 
+
+  }
 
   render () {
     
-    const { title, titleBold, subtitle, btnName, imgBg, imgAlt, products } = this.props;
+    const { products, slides } = this.props;
+    const { activeSlideSmall, activeSlidePage, fade } = this.state;
+
     const hotDealProducts = products.filter(item => item.hotDeals === true);
 
-    const dots = [];
-    for (let i = 0; i < 3; i++) {
-      dots.push(
+    const hotDealCount = hotDealProducts.length;
+
+    const slidesCount = slides.length;
+    console.log('slidesCount', slidesCount);
+
+
+    const dotsSlides = [];
+    for (let i = 0; i < hotDealCount; i++) {
+      dotsSlides.push(
         <li>
-          <a>
-            {/*page {i}  */}
+          <a
+          onClick={() => this.handleSlideSmChange(i)}
+          className={i === activeSlideSmall && styles.active}>
+            page {i}
           </a>
         </li>
       );
@@ -64,43 +101,106 @@ class FeaturedProducts extends React.Component {
                 <div>HOT DEALS</div>
 
                 <div className={'col-auto ' + styles.dots}>
-                  <ul>{dots}</ul>
+                  <ul>{dotsSlides}</ul>
                 </div>
               </div>
               
-              <div>
-                {hotDealProducts.map(item => (
-                  <FeaturedProductsBox key ={item.id} {...item}/>
+              <div className='row'>
+              
+                {hotDealProducts
+                .slice(activeSlideSmall * 1, (activeSlideSmall + 1) * 1)
+                .map(item => (
+                
+                <div key ={item.id} className='col-12'>
+                  <FeaturedProductsBox  {...item}/>
+                  </div>
                 ))}
-                </div>
+              </div>
+              
             </div>
 
             <div className='col-8 col-md-8'>
-              <div className={styles.shopNow}>
 
-                <img src={imgBg} alt={imgAlt} />
+              
+              <div className='row'>
+
+              {/*<div className={styles.shopNow}>  
+
+              <div className='col-12'>*/}
+
+              {slides
+              .slice(activeSlidePage, activeSlidePage + 1)
+              .map(item => ( 
+                
+                <CSSTransition
+                    key={item.id}
+                    timeout={3000}
+                    classNames='fade'
+                    appear={true}
+                    exit={false}
+                  >
+
+
+                <div key ={item.id} className={styles.shopNow}>
+
+                <img src={item.image} alt={item.imgAlt} />
 
                 <div className={styles.shadow}>
-                  <div className={styles.title}>{title} <span>{titleBold}</span></div>
-                  <div className={styles.subtitle}>{subtitle}</div>
+                  <div className={styles.title}>{item.title} <span>{item.titleBold}</span></div>
+                  <div className={styles.subtitle}>{item.subtitle}</div>
                 </div>
                 
                 <div className={styles.buttons}>
                   <Button variant="white">
-                    {btnName}
+                    {item.btnName}
                   </Button>
                 </div>
-      
-                <div className={styles.arrow}>
-                  <div className={styles.arrowLeft}>
-                    <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
-                  </div>
-                  <div className={styles.arrowRight}>
-                    <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
-                  </div>
+
                 </div>
-              </div>
+
+                </CSSTransition>
+
+
+
+              ))}
+
+             {/* </div>
+
+
+               </div> */}
+
+
+              </div> 
+
+
+                {/* BUTTONS */}
+
+                <div className={styles.arrowWrapper}>
+                  <Button variant="arrow"
+                  onClick={e => {
+                    e.preventDefault();
+                    this.handleSlidePageChange(e, activeSlidePage - 1)}}
+                  >
+                    <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+                  </Button>
+                  <Button variant="arrow"
+                  onClick={e => {
+                    e.preventDefault();
+                    this.handleSlidePageChange(e, activeSlidePage + 1)}}
+                    >
+                    <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+                  </Button>
+                </div>
+
+
             </div>
+
+
+
+
+
+
+
           </div>
         </div>
       </div>
