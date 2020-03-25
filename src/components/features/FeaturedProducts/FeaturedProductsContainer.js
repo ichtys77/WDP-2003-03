6865efd568c773db68d0332817323969a6e42519
Anna Kuszal <anna.kuszal@o2.pl>
2020-03-25 15:17:34 +0100

    Add 'slides' to props

--- src/components/features/FeaturedProducts/FeaturedProductsContainer.js
+++ src/components/features/FeaturedProducts/FeaturedProductsContainer.js
@@ -2,14 +2,13 @@ import { connect } from 'react-redux';
 
 import FeaturedProducts from './FeaturedProducts';
 
-import { getAll} from '../../../redux/productsRedux.js';
-
+import { getAll } from '../../../redux/productsRedux.js';
+import { getAllSlides } from '../../../redux/slidesRedux.js';
 
 const mapStateToProps = state => ({
-
   products: getAll(state),
+  slides: getAllSlides(state),
 });
-
 
 /*
 const mapDispatchToProps = dispatch => ({
