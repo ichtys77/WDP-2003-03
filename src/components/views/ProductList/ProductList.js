52d2e96237eed92e5b30fd97272d9258a1a6236a
Anna Kuszal <anna.kuszal@o2.pl>
2020-03-28 19:46:03 +0100

    Add div to display page name

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,9 +1,23 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 
-const ProductList = () => <div className={styles.root}>This is ProductList</div>;
+class ProductList extends React.Component {
+  static propTypes = {
+    children: PropTypes.node,
+  };
 
-// ProductList.propTypes = {};
+  render() {
+    const path = window.location.pathname;
+    const pagePath = path.slice(1, path.length).toUpperCase();
+
+    return (
+      <div className={styles.root}>
+        This is ProductList
+        <div className={styles.category}>{pagePath}</div>
+      </div>
+    );
+  }
+}
 
 export default ProductList;
